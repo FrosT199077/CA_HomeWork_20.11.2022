1a0385d2129e768671121da9f00741dc78d99ad8
Sam <118657335+FrosT199077@users.noreply.github.com>
2022-11-26 13:22:25 +0300

    Add files via upload

---
+++ CA_HomeWork_5.14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CA_HomeWork_5_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int sum;
+
+            Console.WriteLine("Таблица умножения ;)");
+
+            while (i <= 9)
+            {
+                sum = i * 9;
+                Console.WriteLine($"9*{i}={sum}");
+                i++;
+            }
+        }
+    }
+}
