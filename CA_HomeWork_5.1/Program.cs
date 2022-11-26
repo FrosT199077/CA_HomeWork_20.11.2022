d5be7ee9723e6fd58f827d071d21cdf8350b761b
Sam <118657335+FrosT199077@users.noreply.github.com>
2022-11-26 13:19:24 +0300

    Add files via upload

---
+++ CA_HomeWork_5.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CA_HomeWork_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int number;
+
+            Console.Write("Введите число :");
+            number = int.Parse(Console.ReadLine());
+
+            while (i < 10)
+            {
+                Console.Write($"{number} ");
+                i++;
+            }
+        }
+    }
+}
