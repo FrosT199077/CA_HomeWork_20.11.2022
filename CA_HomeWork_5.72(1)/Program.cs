5b90df16dff505765ee5b34e294bbb46e60594e8
Sam <118657335+FrosT199077@users.noreply.github.com>
2022-11-27 00:54:40 +0300

    Add files via upload

---
+++ CA_HomeWork_5.72(1)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CA_HomeWork_5._72_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int firstDay = 9;
+            int sum = 0;
+
+            Console.WriteLine("За сколько дней пробег лыжника превысит 20 км ? ");
+
+            while(i<=12)
+            {
+               
+                sum = firstDay + i;
+                Console.WriteLine($"Лыжник пробежит за {i} дней == {sum} км.");  
+                i++;
+            }
+            Console.WriteLine("Лыжник превысит 20 км - на 12 день .");
+        }
+    }
+}
