c62434f331955adc487cdc3bbd24284e59d3e5de
Sam <118657335+FrosT199077@users.noreply.github.com>
2022-11-27 11:40:42 +0300

    Add files via upload

---
+++ CA_HomeWork_5.72(2)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CA_HomeWork_5._72_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysStart = 0;
+            int daysFinish = 20;
+            double startDist= 10;
+            double finishDist = startDist;
+            double percent = 10;
+
+            Console.WriteLine("Сколько суммарно лыжник пробежит за 20 дней ?");
+
+            while(daysStart<daysFinish)
+            {
+                startDist += startDist*percent/100.0;   
+                finishDist += startDist;
+                daysStart++;
+            }
+            Console.WriteLine($"Лыжник за {daysStart} дней пробежит {finishDist} км.");
+        }
+    }
+}
---
+++ CA_HomeWork_5.72(3)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CA_HomeWork_5._72_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startDays = 0;
+            int finishDays = 10;
+            double firstDist = 10;
+            double lastDist = firstDist;
+            double percent = 10;
+
+            Console.WriteLine("Таблица пробега лыжника за 10 дней !");
+
+            while (startDays < finishDays)
+            {
+                firstDist += firstDist * percent / 100.0;
+                lastDist += firstDist;
+                startDays++;
+                Console.WriteLine($"День: {startDays} == Км.: {firstDist}");
+            }
+            Console.WriteLine($"Сумма пробега за : {startDays} дней == {lastDist} Км.");
+        }
+    }
+}
