6eca8b8eb1e42de79fac9bfb8be0f06ca9bdb3ef
Sam <118657335+FrosT199077@users.noreply.github.com>
2022-11-28 13:30:54 +0300

    Add files via upload

---
+++ CA_HomeWork_DigitsCounter/CA_HomeWork_DigitsCounter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CA_HomeWork_DigitsCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int num = 0;
+            int count5 = 0;
+            int count7 = 0;
+
+            while (i < 10)
+            {
+                Console.WriteLine("Введите числа в рандомном порядке , проверим чего больше 5 или 7 :");
+                num = int.Parse(Console.ReadLine());
+                if (num == 7)
+                {
+                    count7++;
+                }
+                if (num == 5)
+                {
+                    count5++;
+                }
+                i++;
+            }
+
+            if (count5 < count7)
+            {
+                Console.WriteLine("Чисел 7 больше ! ");
+            }
+            else if (count5 > count7)
+            {
+                Console.WriteLine("Чисел 5 больше ! ");
+            }
+            else if (count5 == count7)
+            {
+                Console.WriteLine("Чисел 5 и 7 равное количество !");
+            }
+        }
+    }
+}
